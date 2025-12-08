b774169ea7873defc46a4f66e1ecdef972f452e1
caretechpartners-cell <caretech.partners@gmail.com>
2025-12-08 23:06:08 +0900

    Add files via upload

--- api/login.js
+++ api/login.js
@@ -1,6 +1,6 @@
 const { createClient } = require("@supabase/supabase-js");
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   try {
     res.status(200).json({
       ok: true,
@@ -12,4 +12,4 @@ export default async function handler(req, res) {
       detail: err.message,
     });
   }
-}
+};
